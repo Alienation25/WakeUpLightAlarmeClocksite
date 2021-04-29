bcbf9e1502d009e00b186395a9b55dc50592c9e4
Alienation25 <yaeisvet2000@gmail.com>
2021-04-29 13:59:10 +0300

    Url in html

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views #доступны все функции с файла views.
 
 
 urlpatterns = [
-    path('', views.main_site, name='main_site'), 
+    path('home/', views.main_site, name='main_site'), 
     path('economic/',views.economic_info, name='economic_info'),#www.google.com/economic
     path('document/',views.doc_info,name='doc_info'),
     path('contact/',views.contact_info,name='contact'),
