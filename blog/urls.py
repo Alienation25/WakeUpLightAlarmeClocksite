e4ea84c09d7ea211ad0c5ef7a5143a28ca4c6617
Alienation25 <yaeisvet2000@gmail.com>
2021-04-29 23:16:31 +0300

    create get

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('economic/',views.economic_info, name='economic_info'),#www.google.com/economic
     path('document/',views.doc_info,name='doc_info'),
     path('contact/',views.contact_info,name='contact'),
+    
+    #GET первый шаг
+    path('test_page_HttpResponse/',views.test_GET_HttpResponse,name='test_page_HttpResponse'),# единичкная функция 
+    path('test_page_render/',views.test_GET_render,name='test_page_render'),#работа с целой страницей 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse # нужно для HttpResponse
 from django.utils import timezone
 from .models import Post # работа с бд и с шаблонами базы данных 
 
@@ -15,3 +16,17 @@ def economic_info(request):
 
 def contact_info(request):
     return render(request,'contact_info/index.html',{}) 
+
+
+#второй шаг 
+def test_GET_HttpResponse(request):               
+    msg=request.GET['message_contact_HttpResponse']# перехват get 
+    return HttpResponse(msg)                       # возращение значения(вывод страничкм)
+
+
+def test_GET_render(request):
+    msg=request.GET['message_contact_render'] # перехват get
+    word = len(msg.split())                   # длина слов
+    return render(request,'test_page/index.html',{'word':word,'msg':msg}) #вывод странички и передача значений на страничку 
+
+
